9aaacbe35cff4387c5e22a0fb92a75bdccb5a551
Gsalive <96071769+Gsalive@users.noreply.github.com>
2022-05-14 14:53:39 +0530

    Add files via upload

---
+++ null.c
@@ -0,0 +1,22 @@
+void f1(struct fred_t *p ){
+int x = ->x;
+if(p)
+di_something(x);
+}
+void f2(){
+const char *p =NULL;
+for(int i =0 ; str[i] !='\0' ; i++){
+if(str[i]==''){
+p =str +i;
+break;
+}
+}
+ return p[1];
+}
+void f3(int a )
+{
+struct fred_t *p = NULL 
+if ( a ==1)
+p=fred1;
+ p->x=0;
+}
